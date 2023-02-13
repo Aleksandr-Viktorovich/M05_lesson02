29d6958fbdfef77c36ce19960c3036ff900a0ac6
Aleksandr-Viktorovich <teanvkz@gmail.com>
2023-02-14 03:25:32 +0700

    получение элементовÑ

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const title = document.querySelector('.title');
+const userId = document.querySelector('.form__id');
+const userIdBtn = document.querySelector('.form__btn-user');
+const form = document.querySelector('.form');
+const checked = document.querySelector('.form__check-discount');
+const checkedDiscount = document.querySelector('.form__name-checked');
+const totalPrice = document.querySelector('.form__total-price');
